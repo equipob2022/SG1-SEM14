111819271b6d484882d0b0f19ad5266dff72e62b
equipob2022 <119902400+equipob2022@users.noreply.github.com>
2022-12-22 01:34:30 -0500

    Update model1.py

--- apps/model1.py
+++ apps/model1.py
@@ -13,3 +13,32 @@ from nltk.corpus import stopwords
 nltk.download("stopwords")
 def app():
     st.title('Model 1 - SVR')
+    import snscrape.modules.twitter as sntwitter
+    # query = "(from:BarackObama) until:2022-01-01 since:2002-01-01"
+    query = "(from:PedroCastilloTe) until:2022-12-14 since:2010-01-01"
+    tweets = []
+    limit = 500
+    
+    for tweet in sntwitter.TwitterSearchScraper(query).get_items():
+    if len(tweets) == limit:
+        break
+    else:
+        tweets.append([tweet.date.date(), tweet.content]) 
+        
+    df = pd.DataFrame(tweets, columns=['Date', 'Tweet'])
+    
+    def clean_text(text):  
+        pat1 = r'@[^ ]+'                   #signs
+        pat2 = r'https?://[A-Za-z0-9./]+'  #links
+        pat3 = r'\'s'                      #floating s's
+        pat4 = r'\#\w+'                     # hashtags
+        pat5 = r'&amp '
+        #pat6 = r'[^A-Za-z\s]'         #remove non-alphabet
+        combined_pat = r'|'.join((pat1, pat2,pat3,pat4,pat5))
+        text = re.sub(combined_pat,"",text).lower()
+        return text.strip()
+    #limpiar tweets
+    df['Tweet']=df['Tweet'].apply(clean_text)
+
+    #mostrar los tweets limpiados
+    st.write(df) 
