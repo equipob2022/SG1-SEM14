d3249c36ecd56a565c7198264bcc6d384ef4c9da
equipob2022 <119902400+equipob2022@users.noreply.github.com>
2022-12-22 00:43:59 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-//pruebas
+import streamlit as st
+from multiapp import MultiApp
+from apps import home, model1
+
+app = MultiApp()
+
+st.markdown("""
+# Inteligencia de Negocios - Equipo B
+""")
+# Add all your application here
+app.add_app("Home", home.app)
+app.add_app("Tweet", model1.app)
+
+# The main app
+app.run()
